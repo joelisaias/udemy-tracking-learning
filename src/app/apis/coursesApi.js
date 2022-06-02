5584268a27321bb9bf74d2f8c6f274788a016622
Joel Alvarado <jalvarado@conauto.com.ec>
2022-06-02 09:24:39 -0500

    cambio a api github pages

--- src/app/apis/coursesApi.js
+++ src/app/apis/coursesApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { encode } from "js-base64";
 
 //const { UDEMI_API_URL: baseUrl, UDEMI_API_ID, UDEMI_API_SECRET } = import.meta.env;
-const token = encode( "58CWdGvq6It83jvF4gVKcNIVOBxrfgniq2fTxiBq:FQZ3RofxpDx2KXgwCyX6uz2LExowcEy33P2tqc5SlHqyLmTGluJ6i8CjIT6FRi50j8HNMmrlq6RtUKvJgfknO83t0K2CJsL4gQ8KOTFr8GZdNF0CPAaKP7yyWDZxAB3k" );
+const token = encode( "8FwUGhSfaxIs095h52v4Z8r2V9qKbABPDOYHYPbT:e1Q5yDxfz6TNuUjvkHqQ5yUmucn6qgGEg3faB8gOoQV7ZeyP09NR4hPqMWkc5qp5Ew1Dd7iH3sxqYon0xrNGR2DwY9cnm2EKy6yGGmKf9v4FXKuOFBMYmWwuLuwl0CRT" );
 //console.log('baseUrl',baseUrl);
 console.log(import.meta.env)
 //console.log('client id',import.meta.env.UDEMI_API_ID);
